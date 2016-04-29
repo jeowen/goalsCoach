a6a92aac69a1a860ebfb6247c60f4c36f7ecbfcb
jason.owen@va.gov <jason.owen@va.gov>
2016-04-29 14:17:47 -0700

    Initial Commit

---
+++ goalsCoach/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  goalsCoach
+//
+//  Created by Jason Owen on 4/29/16.
+//  Copyright © 2016 Jason Owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
