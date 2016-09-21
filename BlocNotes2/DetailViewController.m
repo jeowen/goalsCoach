2fa08937aba317dd60fd1e0e7f85999b2c1f2703
jason.owen@va.gov <jason.owen@va.gov>
2016-09-20 21:30:23 -0700

    trivial updates

--- BlocNotes2/DetailViewController.m
+++ BlocNotes2/DetailViewController.m
@@ -47,110 +47,110 @@
 - (IBAction)goalValueTouchUpInside:(id)sender {
     // THIS IS WHERE WE STORE SLIDE TO CORE DATA
     NSLog(@"\n\nTouchUpInside detected ++++++++++>>>>>");
-    NSString * tempString = @"touchUpInside";
-    [self manageGoalUpdates:tempString];
+    // NSString * tempString = @"touchUpInside";
+   //  [self manageGoalUpdates:tempString];
     
 }
 - (IBAction)goalValueTouchUpOutside:(id)sender {
     // THIS IS WHERE WE STORE SLIDE TO CORE DATA
     NSLog(@"\n\nTouchUpOutside detected ******>>>>>");
-    NSString * tempString = @"touchUpOutside";
-    [self manageGoalUpdates:tempString];
+   //  NSString * tempString = @"touchUpOutside";
+   //  [self manageGoalUpdates:tempString];
 }
 
 - (IBAction)goalValueEditingDidEnd:(id)sender {
-    NSString * tempString = @"editingDidEnd";
-    [self manageGoalUpdates:tempString];
+    // NSString * tempString = @"editingDidEnd";
+    // [self manageGoalUpdates:tempString];
 }
 
-#pragma mark- manageGoalUpdates
-- (void)manageGoalUpdates:(NSString *)passedText{
-    // need jsonDict.  get text from core data
-    // convert text into json Dict
-    NSString *jsonString = self.detailItem.updates;
-    NSLog(@"###MADE IT TO manageGoalUpdates### from: %@\n", passedText);
-    
-    //jsonText should be in format:
-    //  { "updates":[
-    //     {"date":dateString, "value":50},
-    //     {"date":dateString2, "value":70}
-    //  ]
-    //  }
-    
-    // 1. Get sqllite updates string
-    // 2. convert json string to NSDictionary
-    // 3. if entry for YYYMODA = null, add record
-    // 4. if entry is not null, replace record
-    // 5. convert NSDictionary to NSString
-    // 6. update in core data
-    
-    NSDictionary *json;
-    
-    if (!([jsonString length] == 0)){
-        NSError *jsonError;
-        NSData *objectData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
-        json = [NSJSONSerialization JSONObjectWithData:objectData
-                                               options:NSJSONReadingMutableContainers
-                                                 error:&jsonError];
-    }
-    
-    NSArray *allUpdates = [json objectForKey:@"updates"];
-    //  NSDictionary *updateSingle = [allUpdates objectAtIndex:0];
-    
-    
-    
-    // get current date in "13:44:59GMT" format
-    
-    //UTC Date
-    NSDate *currentDate = [[NSDate alloc] init];
-    // convert date NSDate to string:
-    
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"dd-MM-YYYY HH:mm:ss"];
-    NSString *dateString = [dateFormatter stringFromDate:currentDate];
-    
-    NSLog(@"The Date: %@", dateString);
-    
-    
-    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[NSDate date]];
-    NSInteger day = [components day];
-    NSInteger month = [components month];
-    NSInteger year = [components year];
-    
-    NSString *yearMonthDay = [NSString stringWithFormat:@"%d%d%d",year, month, day];
-    int valueOfSlider = self.goalValue.value;
-    
-    
-    int numUpdates = [allUpdates count];
-    int k = 0;
-    
-    bool updateFound = 0;
-    while (k < numUpdates){
-        NSDictionary* updateSingle = [allUpdates objectAtIndex:k];
-        NSNumber *updateValue = [updateSingle objectForKey:@"value"];
-        NSString *updateDate = [updateSingle objectForKey:@"date"];
-        if (updateDate == yearMonthDay){
-            // found a match
-            updateFound = 1;
-            // PICK UP HERE: replace value for this date
-        }
-        if (k == numUpdates -1 ){
-            // this is last row with no updateFound, so add the update
-            // valueOfSlider = value to store
-            // yearMonthDay = date to store
-        }
-        
-    }
-    
-    
-    // iterate through json dictionary to find date = monthDayYear
-    // OVER-WRITE json date: yearMonthDay
-    // OVER-WRITE goalValue: self.goalValue for this dictionary entry
-    
-    // convert updated NSDICTIONARY json back to string
-    
-    // write jsonString back to Core Data
-}
+//#pragma mark- manageGoalUpdates
+//- (void)manageGoalUpdates:(NSString *)passedText{
+//    // need jsonDict.  get text from core data
+//    // convert text into json Dict
+//    NSString *jsonString = self.detailItem.updates;
+//    NSLog(@"###MADE IT TO manageGoalUpdates### from: %@\n", passedText);
+//    
+//    //jsonText should be in format:
+//    //  { "updates":[
+//    //     {"date":dateString, "value":50},
+//    //     {"date":dateString2, "value":70}
+//    //  ]
+//    //  }
+//    
+//    // 1. Get sqllite updates string
+//    // 2. convert json string to NSDictionary
+//    // 3. if entry for YYYMODA = null, add record
+//    // 4. if entry is not null, replace record
+//    // 5. convert NSDictionary to NSString
+//    // 6. update in core data
+//    
+//    NSDictionary *json;
+//    
+//    if (!([jsonString length] == 0)){
+//        NSError *jsonError;
+//        NSData *objectData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+//        json = [NSJSONSerialization JSONObjectWithData:objectData
+//                                               options:NSJSONReadingMutableContainers
+//                                                 error:&jsonError];
+//    }
+//    
+//    NSArray *allUpdates = [json objectForKey:@"updates"];
+//    //  NSDictionary *updateSingle = [allUpdates objectAtIndex:0];
+//    
+//    
+//    
+//    // get current date in "13:44:59GMT" format
+//    
+//    //UTC Date
+//    NSDate *currentDate = [[NSDate alloc] init];
+//    // convert date NSDate to string:
+//    
+//    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+//    [dateFormatter setDateFormat:@"dd-MM-YYYY HH:mm:ss"];
+//    NSString *dateString = [dateFormatter stringFromDate:currentDate];
+//    
+//    NSLog(@"The Date: %@", dateString);
+//    
+//    
+//    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[NSDate date]];
+//    NSInteger day = [components day];
+//    NSInteger month = [components month];
+//    NSInteger year = [components year];
+//    
+//    NSString *yearMonthDay = [NSString stringWithFormat:@"%d%d%d",year, month, day];
+//    int valueOfSlider = self.goalValue.value;
+//    
+//    
+//    int numUpdates = [allUpdates count];
+//    int k = 0;
+//    
+//    bool updateFound = 0;
+//    while (k < numUpdates){
+//        NSDictionary* updateSingle = [allUpdates objectAtIndex:k];
+//        NSNumber *updateValue = [updateSingle objectForKey:@"value"];
+//        NSString *updateDate = [updateSingle objectForKey:@"date"];
+//        if (updateDate == yearMonthDay){
+//            // found a match
+//            updateFound = 1;
+//            // PICK UP HERE: replace value for this date
+//        }
+//        if (k == numUpdates -1 ){
+//            // this is last row with no updateFound, so add the update
+//            // valueOfSlider = value to store
+//            // yearMonthDay = date to store
+//        }
+//        
+//    }
+//    
+//    
+//    // iterate through json dictionary to find date = monthDayYear
+//    // OVER-WRITE json date: yearMonthDay
+//    // OVER-WRITE goalValue: self.goalValue for this dictionary entry
+//    
+//    // convert updated NSDICTIONARY json back to string
+//    
+//    // write jsonString back to Core Data
+//}
 
 #pragma mark- Capture text field
 - (IBAction)textFieldDidChange:(UITextField *)sender {
@@ -229,13 +229,16 @@
 
 - (void) viewWillDisappear:(BOOL)animated {
     
+    // GET THE CONTEXT
     NSManagedObjectContext *context = [self.detailItem managedObjectContext];
     NSEntityDescription *entity = [NSEntityDescription entityForName:@"GoalValue" inManagedObjectContext:context];
     GoalValue *goalValue = (GoalValue *)[NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
     
+    // ASSIGN VALUES TO MEMORY for GoalValue Data Model Entity
     goalValue.date = [NSDate date];
     goalValue.value = @(self.goalValue.value);
     
+    // ASSIGN goalValue to detailItem Data Model Entity
     [self.detailItem addGoalValuesObject:goalValue];
     
     
@@ -244,16 +247,11 @@
     
     
     [super viewWillDisappear:animated];
-    // stage goalName & goalValue & currentDate for saving to Core Data
-    
-    //NSString *existingUpdates = [[self.detailItem valueForKey:@"udpates"] description];
-    //NSString *updatedUpdates = [NSString stringWithFormat:@"%@, %@:%@", existingUpdates, [NSDate date],self.goalValueDisplay.text];
-    //[self.detailItem setValue:updatedUpdates forKey:@"updates"];
-    
-    // save context
+ 
     
     NSError *saveError = nil;
     
+    // SAVE MANAGED OBJECT CONTEXT
     if (![self.detailItem.managedObjectContext save:&saveError]) {
         NSLog(@"Unable to save managed object context.");
         NSLog(@"%@, %@", saveError, saveError.localizedDescription);
